0721952271c4e3048ed617d6fa0d6eac1eeee1e1
Kong Lee <konglee873@gmail.com>
2017-08-02 18:18:39 +0800

    save code

--- MixRuntimeBasicUI/ViewController/ViewController.m
+++ MixRuntimeBasicUI/ViewController/ViewController.m
@@ -107,6 +107,36 @@ void objc_setClassHandler(int (*userSuppliedHandler)(const char *))
     
 }
 
+NSArray *ClassGetSubclasses(Class parentClass)
+{
+    int numClasses = objc_getClassList(NULL, 0);
+    Class *classes = NULL;
+    
+    classes = (Class *)malloc(sizeof(Class) * numClasses);
+    numClasses = objc_getClassList(classes, numClasses);
+    
+    NSMutableArray *result = [NSMutableArray array];
+    for (NSInteger i = 0; i < numClasses; i++)
+    {
+        Class superClass = classes[i];
+        do
+        {
+            superClass = class_getSuperclass(superClass);
+        } while(superClass && superClass != parentClass);
+        
+        if (superClass == nil)
+        {
+            continue;
+        }
+        
+        [result addObject:classes[i]];
+    }
+    
+    free(classes);
+    
+    return result;
+}
+
 
 
 
